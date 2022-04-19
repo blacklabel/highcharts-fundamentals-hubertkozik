0962705b16ef3deed2ef88e3991e4045c1edf06a
Hubert Kozik <hubertkozik@MBP-Hubert.localdomain>
2022-04-19 10:11:23 +0200

    Task is ready to be checked

--- highcharts-api/highcharts-maps/1-points-joinby/main.js
+++ highcharts-api/highcharts-maps/1-points-joinby/main.js
@@ -1,14 +1,29 @@
-Highcharts.mapChart('container', {
+(async () => {
+    const topology = await fetch(
+        'https://code.highcharts.com/mapdata/custom/world.topo.json'
+    ).then(response => response.json());
 
-    series: [{
-        data: [
-            ['POL', 100],
-            ['USA', 90],
-            ['PER', 50],
-            ['TZA', 40],
-            ['AUS', 1]
-        ],
-        joinBy: ['iso-a3']
-    }]
-
-});
+    Highcharts.mapChart('container', {
+        chart: {
+            map: topology,
+            zoomType: 'xy',
+            height: 700
+        },
+        mapView: {
+            projection: {
+                name: 'Miller',
+            }
+        },
+        series: [{
+            data: [
+                ['POL', 100],
+                ['USA', 90],
+                ['PER', 50],
+                ['TZA', 40],
+                ['AUS', 1]
+            ],
+            keys: ['iso-a3', 'value'],
+            joinBy: ['iso-a3']
+        }]
+    });
+})();
