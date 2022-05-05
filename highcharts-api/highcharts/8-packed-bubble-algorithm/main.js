2849e898fcdf58c29a7875ebf3b48446cbf383fa
Hubert Kozik <hubertkozik@MBP-Hubert.localdomain>
2022-05-05 13:26:22 +0200

    Improved part 4 of task 8-packed-bubble-algorithm

--- highcharts-api/highcharts/8-packed-bubble-algorithm/main.js
+++ highcharts-api/highcharts/8-packed-bubble-algorithm/main.js
@@ -1,98 +1,154 @@
 const getData = (min, max, count) => {
-    return Array.from({
-        length: count
-    }, () => Math.floor(Math.random() * (max - min) + min));
-}
+    return Array.from(
+        {
+            length: count,
+        },
+        () => Math.floor(Math.random() * (max - min) + min)
+    );
+};
 
-const getRandomItem = (array) => { 
-    return array[Math.floor(Math.random()*array.length)]; 
-}
+const getRandomItem = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+};
 
 (function (H) {
     function clamp(value, min, max) {
-        return value > min ? value < max ? value : max : min;
+        return value > min ? (value < max ? value : max) : min;
     }
-    H.wrap(H.layouts['reingold-fruchterman'].prototype, 'applyLimitBox', function (proceed, node, box) {        
-        var radius = node.radius;
-        /*
-        TO DO: Consider elastic collision instead of stopping.
-        o' means end position when hitting plotting area edge:
+    H.wrap(
+        H.layouts["reingold-fruchterman"].prototype,
+        "applyLimitBox",
+        function (proceed, node, box) {
+            var radius = node.radius;
 
-        - "inelastic":
-        o
-        \
-        ______
-        |  o'
-        |   \
-        |    \
+            function getDist(a, b) {
+                let y = b.plotX - a.plotX;
+                let x = b.plotY - a.plotY;
 
-        - "elastic"/"bounced":
-        o
-        \
-        ______
-        |  ^
-        | / \
-        |o'  \
+                return Math.sqrt(x * x + y * y);
+            }
 
-        Euler sample:
-        if (plotX < 0) {
-            plotX = 0;
-            dispX *= -1;
+            function getTriangleArea(a, b, c) {
+                const side1 = getDist(a, b),
+                    side2 = getDist(b, c),
+                    side3 = getDist(a, c);
+                // calculate the semi-perimeter
+                const s = (side1 + side2 + side3) / 2;
+
+                //calculate the area
+                const areaValue = Math.sqrt(
+                    s * (s - side1) * (s - side2) * (s - side3)
+                );
+
+                return areaValue;
+            }
+
+            function insideTriangle(p, a, b, c) {
+                triangleArea = getTriangleArea(a, b, c);
+                areaSum = 0;
+                areaSum += getTriangleArea(a, b, p);
+                areaSum += getTriangleArea(a, c, p);
+                areaSum += getTriangleArea(b, c, p);
+
+                if (Math.floor(triangleArea) === Math.floor(areaSum)) {
+                    return true;
+                }
+                return false;
+            }
+
+            const h = box.height,
+                m = box.height / 2, // length of half of base
+                halfOfBase = {
+                    plotX: box.width / 2,
+                    plotY: h,
+                },
+                tA = {
+                    // left bottom corner
+                    plotX: halfOfBase.plotX - m,
+                    plotY: h,
+                },
+                tB = {
+                    // right bottom corner
+                    plotX: halfOfBase.plotX + m,
+                    plotY: h,
+                },
+                tC = {
+                    // middle top corner
+                    plotX: halfOfBase.plotX,
+                    plotY: 0,
+                };
+
+            let a = node.plotX - tA.plotX;
+
+            if (node.plotX > halfOfBase.plotX) {
+                a = tB.plotX - node.plotX;
+            }
+
+            const b = (a * h) / m,
+                D = {
+                    plotX: node.plotX,
+                    plotY: h - b,
+                };
+
+            if (insideTriangle(node, tA, tB, tC)) {
+                const A = (tA.plotY - tC.plotY) / (tA.plotX - tC.plotX),
+                    B = -1,
+                    C =
+                        tA.plotY -
+                        ((tA.plotY - tC.plotY) / (tA.plotX - tC.plotX)) *
+                            tA.plotX,
+                    rightA = (tB.plotY - tC.plotY) / (tB.plotX - tC.plotX),
+                    rightB = -1,
+                    rightC =
+                        tB.plotY -
+                        ((tB.plotY - tC.plotY) / (tB.plotX - tC.plotX)) *
+                            tB.plotX;
+
+                const pL =
+                        Math.abs(A * node.plotX + B * node.plotY + C) /
+                        Math.sqrt(A * A + B * B),
+                    rightpL =
+                        Math.abs(
+                            rightA * node.plotX + rightB * node.plotY + rightC
+                        ) / Math.sqrt(rightA * rightA + rightB * rightB);
+
+                const dX = a,
+                    dY = b,
+                    dL = Math.sqrt(a * a + b * b),
+                    pX = (dY / dL) * pL,
+                    pY = (dX / dL) * pL;
+
+                console.log("node.plotY ", node.plotY);
+                console.log("D.plotY + radius ", D.plotY + radius);
+                console.log("h - radius ", h - radius);
+
+                // Limit X-coordinates:
+                node.plotX = clamp(
+                    node.plotX,
+                    tA.plotX + radius,
+                    tB.plotX + radius
+                );
+                // Limit Y-coordinates:
+                node.plotY = clamp(node.plotY, D.plotY + radius, h - radius);
+            }
         }
-
-        if (plotX > box.width) {
-            plotX = box.width;
-            dispX *= -1;
-        }
-        */
-       
-        const h = box.height,
-            m = box.height,
-
-            halfOfBase = {
-            plotX: box.width / 2, 
-            plotY: h},
-
-            tA = {
-            plotX: halfOfBase.plotX - m,
-            plotY: h
-            },
-
-            tB = {
-            plotX: halfOfBase.plotX + m,
-            plotY: h
-            };
-
-        let a = node.plotX - tA.plotX;
-
-        if(node.plotX > halfOfBase.plotX){
-            a = tB.plotX - node.plotX;
-        }
-
-        const b = (a * h) / m,
-            D = {plotX: node.plotX, plotY: h - b};
-
-        // Limit X-coordinates:
-        node.plotX = clamp(node.plotX, tA.plotX + radius, tB.plotX + radius);
-        // Limit Y-coordinates:
-        node.plotY = clamp(node.plotY, D.plotY + radius, h + radius);
-    });
+    );
 })(Highcharts);
 
-Highcharts.chart('container', {
+Highcharts.chart("container", {
     chart: {
-        type: 'packedbubble',
-        height: 800,
+        type: "packedbubble",
+        height: 1000,
     },
     plotOptions: {
         packedbubble: {
-            minSize: '100%',
-            maxSize: '100%',
-            zMin: 0,
-            zMax: 1000,
-        }
+            minSize: "30%",
+            //maxSize: '80%',
+        },
     },
-    series: [{
-        data: getData(1, 50, 100)
-    },]
+    series: [
+        {
+            data: getData(1, 50, 50),
+        },
+    ],
 });
