d90802040e1cc571aed519b08d739bf3d23dc11b
Hubert Kozik <hubertkozik@macbook-pro-hubert.home>
2022-06-02 16:20:47 +0200

    Improved algorithm exercise/highcharts-8-part2

--- highcharts-api/highcharts/8-packed-bubble-algorithm/main.js
+++ highcharts-api/highcharts/8-packed-bubble-algorithm/main.js
@@ -4,69 +4,38 @@ const getData = (min, max, count) => {
     }, () => Math.floor(Math.random() * (max - min) + min));
 }
 
-const getRandomItem = (array) => { 
-    return array[Math.floor(Math.random()*array.length)]; 
-}
-
 (function (H) {
-    H.wrap(H.layouts.packedbubble.prototype, 'repulsiveForces', function (proceed) {        
-        var layout = this,
-            force,
-            distanceR,
-            distanceXY,
-            bubblePadding = layout.options.bubblePadding;
-        layout.nodes.forEach(function (node) {
-            node.degree = node.mass;
-            node.neighbours = 0;
-            layout.nodes.forEach(function (repNode) {
-                force = 0;
-                if (
-                // Node can not repulse itself:
-                node !== repNode &&
-                    // Only close nodes affect each other:
-                    // Not dragged:
-                    //!node.fixedPosition &&
-                    (layout.options.seriesInteraction ||
-                        node.series === repNode.series)) {
-                    distanceXY = layout.getDistXY(node, repNode);
-                    distanceR = (layout.vectorLength(distanceXY) -
-                        (node.marker.radius +
-                            repNode.marker.radius +
-                            bubblePadding));
-                    // TODO padding configurable
-                    // const dist = 0.15;
-                    // if (distanceR > -1 * dist && distanceR < dist) {
-                    //     const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-                    //     repNode.color = randomColor;
-                    // }
-                    if (distanceR < 0) {
-                        node.degree += 0.01;
-                        node.neighbours++;
-                        force = layout.repulsiveForce(-distanceR / Math.sqrt(node.neighbours), layout.k, node, repNode);
-                    }
-                    if (force > 4) {
-                        const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-                        repNode.color = randomColor;
-                    }
-                    layout.force('repulsive', node, force * repNode.mass, distanceXY, repNode, distanceR);
-                    
+    H.wrap(
+        H.layouts['reingold-fruchterman'].prototype, 'force', function (p) {
+            p.apply(this, Array.prototype.slice.call(arguments, 1));            
+
+            if (arguments[1] === 'repulsive') {
+                const repNode = arguments[5],
+                force = arguments[3] / repNode.mass,
+                forceLimit = 4;
+
+                if (force > forceLimit) {
+                    const randomColor = Math.floor(Math.random() * 16777215);
+                    repNode.color = '#' + randomColor.toString(16);
                 }
-            });
-        });
-    });
+            }
+        }
+    );
+// eslint-disable-next-line no-undef
 })(Highcharts);
 
+// eslint-disable-next-line no-undef
 Highcharts.chart('container', {
     chart: {
         type: 'packedbubble',
-        height: 800,
+        height: 800
     },
     plotOptions: {
         packedbubble: {
             minSize: '20%',
             maxSize: '100%',
             zMin: 0,
-            zMax: 1000,
+            zMax: 1000
         }
     },
     series: [{
